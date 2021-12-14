e1a53a6dc226a85ee8364830dccb1afedd3c4db8
AhmedAliEgypt <76600798+AhmedAliEgypt@users.noreply.github.com>
2021-12-15 01:56:32 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$token = "5081834242:AAFrjhfprZd3xR01wjQGQSHrLFm7zw8paBI";
+$admin = 911000886;
+if(isset($_POST['submit'])){
+$url = $_POST['url'];
+$videoid = $url;
+
+}
+?>
+<html>
+<body>
+<h2>اهلا بك في مراسله المطور سجاد العراقي</h2>
+<div>
+<form method="post"  action="">
+<input type="text"  name="url"  placeholder="قم بكتابه رسالتك هنا " width="250" >
+<div></div>
+    <input type="submit" name="submit" width="250">
+    </form>
+    
+<div class="thumbnail">
+<?php
+if(! empty($videoid)){
+
+
+?>
+<h3>تم ارسال رسالتك</h3>
+
+<?php
+file_get_contents("https://api.telegram.org/bot$token/sendmessage?chat_id=".$admin."&parse_mode=markdown&text=".urlencode($url));
+    
+}
+?>
+</div>
+</div>
+<body>
+<html>
